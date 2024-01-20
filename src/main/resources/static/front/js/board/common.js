7d4b4ad6d665963abb8ff39dc963e8bf00ec90f7
Lee-GiHeung <dlrlgmd25@gamil.com>
2024-01-20 15:06:17 +0900

    게시판 찜 기능 - 진행중

--- src/main/resources/static/front/js/board/common.js
+++ src/main/resources/static/front/js/board/common.js
@@ -1,21 +1,26 @@
 const boardLib = {
     /**
     * 게시글 찜하기
-    *   - 로그읺나 회원 정보를 가지고 저장
+    *   - 로그인과 회원 정보를 가지고 저장
     *
     * @param bSeq : 게시글 번호
     */
     save(bSeq) {
+        const { ajaxLoad } = commonLib;
 
+        ajaxLoad('GET', `/api/board/save_post/${bSeq}`);
 
     },
-
     /**
     * 찜하기 해제
     *
     * @param bSeq : 게시글 번호
     */
     deleteSave(bSeq) {
+
+        const { ajaxLoad } = commonLib;
+
+        ajaxLoad('DELETE', `/api/board/save_post/${bSeq}`);
 
     }
 }
@@ -26,7 +31,14 @@ window.addEventListener("DOMContentLoaded", function() {
     for (const el of savePosts) {
         el.addEventListener("click", function() {
             // data-seq : 게시글 번호
+            // save_post에 on 클래스가 포함되어 있는 경우 -> 찜 한 상태. on -> 찜을 아직 안한 상태
             const bSeq = this.dataset.seq;
+            if (this.classList.contains('on')) { // 찜한 상태 -> 해제
+               boardLib.deleteSave(bSeq);
+
+            } else { // 찜을 아직 안한 상태 -> 찜하기
+                boardLib.save(bSeq);
+            }
 
             boardLib.save(bSeq);
 
