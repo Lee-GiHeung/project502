14e6f23b1b86cbeba1b1cb4ee4f867cc57db35a0
Lee-GiHeung <dlrlgmd25@gamil.com>
2024-01-20 14:13:34 +0900

    게시판 찜 기능 - 진행중

---
+++ src/main/java/org/choongang/board/entities/SaveBoardDataId.java
@@ -0,0 +1,15 @@
+package org.choongang.board.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaveBoardDataId {
+
+    private Long bSeq; // 게시글 번호
+    private Long mSeq; // 회원번호
+
+}
