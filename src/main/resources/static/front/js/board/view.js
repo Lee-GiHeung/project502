b8a3946e9faf36d26fdf78c3cdc8f09dd00a6f8e
Lee-GiHeung <dlrlgmd25@gamil.com>
2024-01-22 14:47:48 +0900

    게시판 댓글 수정 기능 - 진행중

--- src/main/resources/static/front/js/board/view.js
+++ src/main/resources/static/front/js/board/view.js
@@ -13,6 +13,42 @@ window.addEventListener("DOMContentLoaded", function(){
 
     /* 댓글 수정 버튼 클릭 처리 S */
     const editComments = document.getElementsByClassName("edit_comment");
+    for(const el of editComments) {
+        el.addEventListener("click", function() {
+            const dataset = this.dataset;
+            if(this.dataset.editable == 'false') { // 비회원 댓글 -> 비밀번호 확인
+                checkRequiredPassword(dataset.seq);
+
+            }
+        });
+    }
+
+    /**
+    * 비회원 비밀번호 필요한지 체크
+    *
+    * @param seq : 댓글 등록번호
+    * @param success : 비밀번호 검증 이미 완료 된 상태 -> 댓글 수정 textarea 노출
+    * @param failure : 비밀번호 검증 필요 -> 비밀번호 입력 항목 노출
+    */
+    async function checkRequiredPassword(seq, success, failure) {
+        try {
+            const { ajaxLoad } = commonLib;
+
+            const result = await ajaxLoad('GET', `/api/comment/auth_check?seq=${seq}`,
+            null, 'json');
+            console.log(result)
+            success();
+
+        } catch (err) { // 비밀번호 검증 필요
+            console.log(err);
+            failure();
+
+        }
+    }
+
+
+    /*
+    const editComments = document.getElementsByClassName("edit_comment");
     const { ajaxLoad } = commonLib;
 
     for(const el of editComments) {
@@ -69,30 +105,37 @@ window.addEventListener("DOMContentLoaded", function(){
                                 targetEl.appendChild(passwordBox);
                                 targetEl.appendChild(button);
 
-                                const guestPw = passwordBox.value.trim();
-                                if(!guestPw) {
-                                    alert("비밀번호를 입력하세요.");
-                                    passwordBox.focus();
-                                    return;
-                                }
-
                                 button.addEventListener("click", function() {
+
+                                const guestPw = passwordBox.value.trim();
+                                   if(!guestPw) {
+                                       alert("비밀번호를 입력하세요.");
+                                       passwordBox.focus();
+                                       return;
+                                   }
 
                                     ajaxLoad("GET", `/api/comment/auth_check?seq=${seq}&guestPw=${guestPw}`,
                                      null, 'json')
                                         .then(res => {
-                                            targetEl.innerHTML = "";
-                                            textArea.value = data.content;
-                                            targetEl.appendChild(textArea);
+                                            // 비회원 비밀번호 검증 완료 후
+
+//                                            targetEl.innerHTML = "";
+//                                            textArea.value = data.content;
+//                                            targetEl.appendChild(textArea);
 
                                        })
-                                       .catch(err => console.log(err));
+                                       .catch(err => { // 비회원 비밀번호 검증 필요: 비번 확인 항목 노출
+
+                                            targetEl.appendChild(passwordBox);
+                                            targetEl.appendChild(button);
+
+                                       });
 
                                 });
+                            } else {
+                                textArea.value = data.content;
+                                targetEl.appendChild(textArea);
                             }
-
-                            textArea.value = data.content;
-                            targetEl.appendChild(textArea);
 
                         }
                     })
