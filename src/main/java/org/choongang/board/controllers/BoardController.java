0bba24d3ada93e74347c2f4644f584ed1de7937d
Lee-GiHeung <dlrlgmd25@gamil.com>
2024-01-22 17:53:04 +0900

    게시판 답글 기능 완료

--- src/main/java/org/choongang/board/controllers/BoardController.java
+++ src/main/java/org/choongang/board/controllers/BoardController.java
@@ -146,6 +146,7 @@ public class BoardController implements ExceptionProcessor {
         content = String.format
             ("<br><br><br><br><br>============================<br>%s=========", content);
 
+        form.setBid(board.getBid());
         form.setContent(content);
         form.setParentSeq(parentSeq);
 
@@ -248,7 +249,7 @@ public class BoardController implements ExceptionProcessor {
 
         String pageTitle = board.getBName(); // 게시판명이 기본 타이틀
         
-        if(mode.equals("write") || mode.equals("update")) { // 쓰기 또는 수정 
+        if(mode.equals("write") || mode.equals("update") || mode.equals("reply")) { // 쓰기 또는 수정
             if(board.isUseEditor()) { // 에디터 사용하는 경우
                 addCommonScript.add("ckeditor5/ckeditor");
             }
